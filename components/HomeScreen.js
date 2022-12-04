53eef099c861c36c0d279d3085acce8d5ad2d1a9
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-12-04 13:50:12 -0500

    Add recent articles and blogs to homescreen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,9 +1,32 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, SectionList } from 'react-native';
+import { useNews } from '../context/newsContext';
+import Card from './Card';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
+  const [news] = useNews();
+  if (news.length === 0) {
+    return null;
+  }
+  const articlesData = news[0].slice(0, 3);
+  const blogsData = news[1].slice(0, 3);
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home Screen</Text>
+      <SectionList
+        sections={[
+          { title: ' Recent Articles', data: articlesData },
+          { title: ' Recent Blogs', data: blogsData },
+        ]}
+        renderItem={({ item }) => {
+          return (
+            <Card
+              data={item}
+              onPress={() => navigation.navigate('Details', { id: item.id })}
+            />
+          );
+        }}
+        renderSectionHeader={({ section }) => <Text>{section.title}</Text>}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 }
@@ -12,7 +35,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
