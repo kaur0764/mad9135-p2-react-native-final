48273877c983d0c6548792c275a4bcff8077380e
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-12-05 12:29:34 -0500

    Add articles screen content

--- components/ArticlesScreen.js
+++ components/ArticlesScreen.js
@@ -1,9 +1,27 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { useNews } from '../context/newsContext';
+import Card from './Card';
 
 export default function ArticlesScreen({ navigation }) {
+  const [news] = useNews();
+  if (news.length === 0) {
+    return null;
+  }
+  const data = news[0];
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Articles Screen</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Card
+            data={item}
+            onPress={() =>
+              navigation.navigate('Article Details', { item: item })
+            }
+          />
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 }
@@ -12,7 +30,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
