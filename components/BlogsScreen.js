149c43185c1045a8d2a9f975cb2ec370f36d102a
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-12-05 15:15:21 -0500

    Add blogs screen content

--- components/BlogsScreen.js
+++ components/BlogsScreen.js
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { useNews } from '../context/newsContext';
+import Card from './Card';
 
-export default function BlogsScreen() {
+export default function BlogsScreen({ navigation }) {
+  const [news] = useNews();
+  if (news.length === 0) {
+    return null;
+  }
+  const data = news[1];
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Blogs Screen</Text>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Card
+            data={item}
+            onPress={() => navigation.navigate('Blog Details', { item: item })}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 }
@@ -12,7 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
