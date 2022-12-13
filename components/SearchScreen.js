4bea43615a72f7ebcb4284c686cfb1d162032344
JasreetK <90123505+JasreetK@users.noreply.github.com>
2022-12-13 18:43:55 -0500

    Update DetailsScreen and SearchScreen style

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -138,6 +138,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#111',
+    paddingTop: 10,
   },
   searchContainer: {
     marginBottom: 32,
